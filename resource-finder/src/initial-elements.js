b63d03ffa39940b9db6d4db6d502883297ee9c65
landnn <landonnbrownn@gmail.com>
2023-09-27 13:32:08 -0600

    initial project

---
+++ resource-finder/src/initial-elements.js
@@ -0,0 +1,126 @@
+import React from "react";
+import { MarkerType } from "reactflow";
+
+export const nodes = [
+  {
+    id: "1",
+    type: "input",
+    data: {
+      label: (
+        <>
+          What do you need help with?
+        </>
+      )
+    },
+    position: { x: 250, y: 0 },
+    interactive: false,
+  },
+  {
+    id: "2",
+    data: {
+      label: (
+        <>
+          Mental Health
+        </>
+      )
+    },
+    position: { x: -100, y: 100 },
+    interactive: false,
+  },
+  {
+    id: "3",
+    data: {
+      label: (
+        <>
+          Academic/Career Advising
+        </>
+      )
+    },
+    position: { x: 100, y: 100 },
+    style: {
+      background: "#D6D5E6",
+      color: "#333",
+      border: "1px solid #222138",
+      width: 180
+    },
+    interactive: false,
+  },
+  {
+    id: "4",
+    position: { x: 300, y: 100 },
+    data: {
+      label: "Physical Wellness"
+    },
+    interactive: false,
+  },
+  {
+    id: "5",
+    data: {
+      label: "Finding Community"
+    },
+    position: { x: 500, y: 100 },
+    interactive: false,
+  },
+//   {
+//     id: "6",
+//     type: "output",
+//     data: {
+//       label: (
+//         <>
+//           An <strong>output node</strong>
+//         </>
+//       )
+//     },
+//     position: { x: 100, y: 480 }
+//   },
+//   {
+//     id: "7",
+//     type: "output",
+//     data: { label: "Another output node" },
+//     position: { x: 400, y: 450 }
+//   }
+];
+
+export const edges = [
+  { id: "e1-2", 
+    source: "1", 
+    target: "2", 
+    // label: "this is an edge label" 
+  },
+  { id: "e1-3", source: "1", target: "3" },
+  {
+    id: "e1-4",
+    source: "1",
+    target: "4",
+    animated: false,
+    // label: "animated edge",
+    interactive: false,
+  },
+  {
+    id: "e1-5",
+    source: "1",
+    target: "5",
+    // label: "edge with arrow head",
+    // markerEnd: {
+    //   type: MarkerType.ArrowClosed
+    // },
+    interactive: false,
+  },
+//   {
+//     id: "e5-6",
+//     source: "5",
+//     target: "6",
+//     type: "smoothstep",
+//     label: "smooth step edge"
+//   },
+//   {
+//     id: "e5-7",
+//     source: "5",
+//     target: "7",
+//     type: "step",
+//     style: { stroke: "#f6ab6c" },
+//     label: "a step edge",
+//     animated: true,
+//     labelStyle: { fill: "#f6ab6c", fontWeight: 700 }
+//   }
+];
